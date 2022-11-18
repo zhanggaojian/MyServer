a2a6a74318184bf4d2800ed8a910d5fb1136e5cc
zhanggaojian <zhanggaojian@xiaomi.com>
2022-11-18 19:15:05 +0800

    update

--- pool/sqlconnpool.cpp
+++ pool/sqlconnpool.cpp
@@ -22,6 +22,41 @@ void SqlConnPool::SqlInit(string host,int port,string user,string pwd,string dbn
     m_databasename=dbname;
     m_maxConn=connSize;
 
-    
+    //创建数据库连接
+    for(int i=0;i<m_maxConn;i++){
+        MYSQL* con=nullptr;
+
+        con=mysql_init(con);
+
+        if(con==nullptr){
+            std::cout<<"MYSQL Error"<<endl;
+            exit(1);//退出进程，只要不是0，都是异常退出，是0就是正常退出。
+        }
+
+        con=mysql_real_connect(con,m_host.c_str(),m_user.c_str(),m_passwd.c_str(),m_databasename.c_str(),port,nullptr,0);
+
+        if(con==nullptr){
+            std::cout<<"MYSQL Error"<<std::endl;
+            exit(1);
+        }
+
+        connList.push_back(con);
+        ++m_FreeConn;
+    }
+    m_maxConn = m_FreeConn;
+}
+
+MYSQL* SqlConnPool::GetConn(){
+    MYSQL* rescon=nullptr;
+
+    if(connList.size()==0) return nullptr;
+
+    rescon = connList.front();
+    connList.pop_front();
+
+    --m_FreeConn;
+    ++m_curConn;
+
+    return rescon;
 }
 
--- pool/sqlconnpool.h
+++ pool/sqlconnpool.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <mysql/mysql.h>
+#include <list>
 using namespace std;
 
 class SqlConnPool{
@@ -11,9 +12,9 @@ public:
     static SqlConnPool* GetInstance();
 
     //sql init
-    void SqlInit(string host,int port,string user,string pwd,string database,int connSize);
+    void SqlInit(string host,int port,string user,string pwd,string dbname,int connSize);
 
-    //获取数据库连接
+    //从数据库连接池中获取一个可用的数据库连接
     MYSQL *GetConn();
 
     //释放数据库连接
@@ -31,6 +32,8 @@ private:
     int m_maxConn;//最大连接数
     int m_curConn;//当前使用的连接数
     int m_FreeConn;//剩余的连接数
+
+    list<MYSQL *> connList; 
 public:
     string m_host;//主机地址
     string m_user;//数据库登录用户名
